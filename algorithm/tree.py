7ee8262250bb802abdfa7b19437cc550ebac7796
Julien St-Pierre Fortin <julien.stpierre-fortin@gameloft.com>
2018-12-04 11:58:49 -0500

    Move some logic to data structures

--- algorithm/tree.py
+++ algorithm/tree.py
@@ -1,36 +1,20 @@
 # Binary tree algorithms
 def in_order_traversal(node):
-	if node:
-		in_order_traversal(node.left_child)
-		print('Visiting node with value {}'.format(node.value))
-		in_order_traversal(node.right_child)
+    if node:
+        in_order_traversal(node.left_child)
+        print('Visiting node with value {}'.format(node.value))
+        in_order_traversal(node.right_child)
 
 def pre_order_traversal(node):
-	if node:
-		print('Visiting node with value {}'.format(node.value))
-		pre_order_traversal(node.left_child)
-		pre_order_traversal(node.right_child)
+    if node:
+        print('Visiting node with value {}'.format(node.value))
+        pre_order_traversal(node.left_child)
+        pre_order_traversal(node.right_child)
 
 
 def post_order_traversal(node):
-	if node:
-		post_order_traversal(node.left_child)
-		post_order_traversal(node.right_child)
-		print('Visiting node with value {}'.format(node.value))
+    if node:
+        post_order_traversal(node.left_child)
+        post_order_traversal(node.right_child)
+        print('Visiting node with value {}'.format(node.value))
 
-def node_count(node):
-	if not node:
-		return 0
-	return (1 + node_count(node.left_child) + node_count(node.right_child))
-
-def is_complete(node):
-	nb_of_nodes = node_count(node)
-	idx = 0
-	def _is_complete(node, idx, nb_of_nodes):
-		if not node:
-			return True
-		if idx >= nb_of_nodes:
-			return False
-		return (_is_complete(node.left_child, 2*idx+1, nb_of_nodes) and 
-			_is_complete(node.right_child, 2*idx+2, nb_of_nodes))
-	return _is_complete(node, idx, nb_of_nodes)
