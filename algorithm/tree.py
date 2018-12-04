c563afaf756c0bf637a584d564b0d65cd6333539
Julien St-Pierre Fortin <julien.stpierre-fortin@gameloft.com>
2018-12-04 17:00:48 -0500

    Add tree rotation algorithms

--- algorithm/tree.py
+++ algorithm/tree.py
@@ -18,3 +18,27 @@ def post_order_traversal(node):
         post_order_traversal(node.right_child)
         print('Visiting node with value {}'.format(node.value))
 
+def right_rotation(node):
+    if node:
+        if node.left_child:
+            temp_node = node
+            temp_grand_child = node.left_child.right_child
+
+            node = node.left_child
+            node.right_child = temp_node
+            if temp_grand_child:
+                node.right_child.left_child = temp_grand_child
+    return node
+
+
+def left_rotation(node):
+    if node:
+        if node.right_child:
+            temp_node = node
+            temp_grand_child = node.right_child.left_child
+
+            node = node.right_child
+            node.left_child = temp_node
+            if temp_grand_child:
+                node.left_child.right_child = temp_grand_child
+    return node
