b86e8ef12b54e293fc995b73682dac52a273941b
Julien St-Pierre Fortin <julien.stpierre-fortin@gameloft.com>
2018-12-04 11:59:37 -0500

    Add untested maxheap and minheap data structures

--- data_structure/tree.py
+++ data_structure/tree.py
@@ -1,3 +1,27 @@
+def _node_count(node):
+    if not node:
+        return 0
+    return (1 + _node_count(node.left_child) + _node_count(node.right_child))
+
+def _is_complete(node):
+    nb_of_nodes = _node_count(node)
+    idx = 0
+    def _is_comp(node, idx, nb_of_nodes):
+        if not node:
+            return True
+        if idx >= nb_of_nodes:
+            return False
+        return (_is_comp(node.left_child, 2*idx+1, nb_of_nodes) and 
+            _is_comp(node.right_child, 2*idx+2, nb_of_nodes))
+    return _is_comp(node, idx, nb_of_nodes)
+
+def _swap_array_elements(array, idx_1, idx_2):
+    tmp = array[idx_1]
+    array[idx_1] = array[idx_2]
+    array[idx_2] = tmp
+    return array
+
+
 class TreeNode:
     def __init__(self, value, **children):
         self.value = value
@@ -19,15 +43,17 @@ class BinaryTree(Tree):
     def __init__(self, root_node):
         super().__init__(root_node)
 
-class CompleteBinaryTree(BinaryTree):
+
+class CompleteBinaryTree(Tree):
     def __init__(self, root_node):
         super().__init__(root_node)
     def is_complete(self):
-        pass
+        return _is_complete(self.root_node)
 
 class BinarySearchTree(BinaryTree):
     def __init__(self, root_node):
         super().__init__(root_node)
+        self._node_count = 1
 
     def insert(self, value):
         print("Inserting node with value {}".format(value))
@@ -50,13 +76,114 @@ class BinarySearchTree(BinaryTree):
             self.root_node = node
 
 
-class BinaryHeap(CompleteBinaryTree):
+class _BinaryHeap(CompleteBinaryTree):
+    def __init__(self, root_node=None):
+        super().__init__(root_node)
+        self._array_repr = [0]
+        self._array_repr_size = 0
+
+    def insert(self, value):
+        # Ensure shape property
+        self._array_repr.append(value)
+        self._array_repr_size += 1
+        # Ensure heap property
+        self._percolate_up_last_node()
+
+    def get_extremum(self):
+        return self._array_repr[1]
+
+    def delete_extremum(self):
+        old = self._array_repr[1]
+        self._array_repr[1] = self._array_repr.pop()
+        self._array_repr_size -= 1
+        self._percolate_down_last_node()       
+        return old
+
+    def _percolate_up_last_node(self):
+        pass
+
+    def _percolate_down_last_node(self):
+        pass
+
+class MinHeap(_BinaryHeap):
+
+    def _get_min_child(self, idx):
+        if 2*idx+1 > self._array_repr_size:
+            return 2*idx
+        else:
+            if self._array_repr[2*idx] > self._array_repr[2*idx+1]:
+                return 2*idx + 1
+            else:
+                return 2*idx
+
+    def _percolate_up_last_node(self):
+        idx = self._array_repr_sizer
+        while idx > 1:
+            parent_idx = idx//2
+            if self._array_repr[parent_idx] > self._array_repr[idx]:
+                self._array_repr = _swap_array_elements(self._array_repr, 
+                                                        parent_idx, 
+                                                        idx)
+            idx //= 2
+
+    def _percolate_down_first_node(self):
+        idx = 1
+        while idx < self._array_repr_size:
+            min_child_idx = self._get_min_child(idx)
+            if self._array_repr[min_child_idx] < self._array_repr[idx]:
+                self._array_repr = _swap_array_elements(self._array_repr, 
+                                                        min_child_idx, 
+                                                        idx)
+            idx = min_child_idx
+
+
+    get_min = _BinaryHeap.get_extremum
+    delete_min = _BinaryHeap.delete_extremum
+
+
+
+class MaxHeap(_BinaryHeap):
+    def _get_max_child(self, idx):
+        if 2*idx+1 > self._array_repr_size:
+            return 2*idx
+        else:
+            if self._array_repr[2*idx] < self._array_repr[2*idx+1]:
+                return 2*idx + 1
+            else:
+                return 2*idx
+
+    def _percolate_up_last_node(self):
+        idx = self._array_repr_sizer
+        while idx > 1:
+            parent_idx = idx//2
+            if self._array_repr[parent_idx] < self._array_repr[idx]:
+                self._array_repr = _swap_array_elements(self._array_repr, 
+                                                        parent_idx, 
+                                                        idx)
+            idx //= 2
+
+    def _percolate_down_first_node(self):
+        idx = 1
+        while idx < self._array_repr_size:
+            max_child_idx = self._get_max_child(idx)
+            if self._array_repr[max_child_idx] > self._array_repr[idx]:
+                self._array_repr = _swap_array_elements(self._array_repr, 
+                                                        min_child_idx, 
+                                                        idx)
+            idx = max_child_idx
+
+    get_max = _BinaryHeap.get_extremum
+    delete_max = _BinaryHeap.delete_extremum
+
+
+
+class AVLTree(BinaryTree):
     pass
 
-class MinHeap(BinaryHeap):
+
+class RedBlackTree(BinaryTree):
     pass
 
-class MaxHeap(BinaryHeap):
+
+class SplayTree(BinaryTree):
     pass
-
-
