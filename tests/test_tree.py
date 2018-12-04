b9de85a05e984cf3bdc3d4a6eccfd8a604b2df9b
Julien St-Pierre Fortin <julien.stpierre-fortin@gameloft.com>
2018-12-04 12:14:40 -0500

    Add binary heap tests

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,11 +1,17 @@
 import unittest
 
-from data_structure.tree import BinaryTreeNode, BinaryTree, BinarySearchTree
+from data_structure.tree import (BinaryTreeNode, 
+                                 BinaryTree, 
+                                 BinarySearchTree,
+                                 MinHeap,
+                                 MaxHeap,
+                                 _node_count,
+                                 _is_complete)
 from algorithm.tree import (in_order_traversal, 
                               pre_order_traversal,
-                              post_order_traversal,
-                              node_count,
-                              is_complete)
+                              post_order_traversal)
+                              # node_count,
+                              # is_complete)
 
 def get_mock_binary_tree():
     n1 = BinaryTreeNode(1, None, None)
@@ -20,6 +26,22 @@ def get_mock_binary_tree():
 def get_mock_binary_search_tree():
     n1 = BinaryTreeNode(7, None, None)
     return BinarySearchTree(n1)
+
+def get_mock_min_heap():
+    return get_mock_heap(MinHeap)
+
+def get_mock_max_heap():
+    return get_mock_heap(MaxHeap)
+
+def get_mock_heap(heap_type):
+    mh = heap_type()
+    mh.insert(2)
+    mh.insert(30)
+    mh.insert(5)
+    mh.insert(200)
+    mh.insert(3)
+    mh.insert(10)
+    return mh
 
 
 class TestBinaryTree(unittest.TestCase):
@@ -44,13 +66,13 @@ class TestBinaryTree(unittest.TestCase):
     def test_node_count(self):
         print("Testing node_count...")
         tree = get_mock_binary_tree()
-        assert(node_count(tree.root_node)==7)  
+        assert(_node_count(tree.root_node)==7)  
         print()
 
     def test_is_complete(self):
         print("Testing is_complete...")
         tree = get_mock_binary_tree()
-        assert(is_complete(tree.root_node))  
+        assert(_is_complete(tree.root_node))  
         print()
 
     def test_binary_search_tree_insert(self):
@@ -63,4 +85,36 @@ class TestBinaryTree(unittest.TestCase):
         binary_search_tree.insert(3)
         binary_search_tree.insert(10)
         in_order_traversal(binary_search_tree.root_node)
+        print()
+
+
+class TestBinaryHeap(unittest.TestCase):
+    def test_min_heap_get_min(self):
+        print("Testing min_heap get_min")
+        mh = get_mock_min_heap()
+        print(mh._array_repr[1:])
+        print(mh.get_min())
+        print(mh._array_repr[1:])
+        print()
+
+    def test_max_heap_get_max(self):
+        print("Testing min_heap get_max")
+        mh = get_mock_max_heap()
+        print(mh.get_max())
+        print()
+
+    def test_min_heap_get_min(self):
+        print("Testing min_heap delete_min")
+        mh = get_mock_min_heap()
+        print(mh._array_repr[1:])
+        print(mh.delete_min())
+        print(mh._array_repr[1:])
+        print()
+
+    def test_max_heap_get_max(self):
+        print("Testing max_heap delete_max")
+        mh = get_mock_max_heap()
+        print(mh._array_repr[1:])
+        print(mh.delete_max())
+        print(mh._array_repr[1:])
         print()
