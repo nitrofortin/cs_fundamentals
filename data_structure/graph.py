d01e90bac05681f41e991c87fcd8cf25da28540f
Julien St-Pierre Fortin <julien.stpierre-fortin@gameloft.com>
2018-12-06 09:54:59 -0500

    Update graph ds

--- data_structure/graph.py
+++ data_structure/graph.py
@@ -2,8 +2,8 @@ class GraphException(Exception):
     pass
 
 class GraphNode(object):
-    def __init__(self, value=None):
-        self.value = value
+    def __init__(self, id=None):
+        self.id = id
         self.neighbors = {}
 
     def add_neighbor(self, node, weight=1):
@@ -15,14 +15,14 @@ class GraphNode(object):
     def get_neighbors(self):
         return list(self.neighbors.keys())
 
-    def __contains__(self, node_value):
-        return (node_value in self.neighbors.keys())
+    def __contains__(self, node_id):
+        return (node_id in self.neighbors.keys())
 
 
 
 class NullNode(GraphNode):
-    def __init__(self, value=None):
-        self.value = None
+    def __init__(self, id=None):
+        self.id = None
     
 
 class Graph(object):
@@ -31,39 +31,42 @@ class Graph(object):
         self.number_of_nodes = 0
         self._recovering_state = self
 
-    def add_node(self, node_value):
-        n = GraphNode(node_value)
-        if node_value in self.nodes.keys():
-            print('Node {} already exists'.format(node_value))
+    def add_node(self, node_id):
+        n = GraphNode(node_id)
+        if node_id in self.nodes.keys():
+            print('Node {} already exists'.format(node_id))
         else:
             self.number_of_nodes += 1
-            self.nodes[node_value] = n
+            self.nodes[node_id] = n
         
-    def add_edge(self, source, origin, weight=1):
-        if source not in self.nodes.keys() or origin not in self.nodes.keys():
+    def add_edge(self, source, target, weight=1):
+        if source not in self.nodes.keys() or target not in self.nodes.keys():
             raise GraphException('Node not in graph nodes')
         node_source = self.nodes[source]
-        node_origin = self.nodes[origin]
-        node_source.add_neighbor(origin, weight)
-        node_origin.add_neighbor(source, weight)
+        node_target = self.nodes[target]
+        node_source.add_neighbor(node_target, weight)
+        node_target.add_neighbor(node_source, weight)
 
-    def add_directed_edge(self, source, origin, weight=1):
-        if source not in self.nodes.keys() or origin not in self.nodes.keys():
+    def add_directed_edge(self, source, target, weight=1):
+        if source not in self.nodes.keys() or target not in self.nodes.keys():
             raise GraphException('Node not in graph nodes')
         node_source = self.nodes[source]
-        node_source.add_neighbor(origin, weight)
+        node_target = self.nodes[target]
+        node_source.add_neighbor(node_target, weight)
 
-    def get_node(self, node_value):
+    def get_node(self, node_id):
         for n in self.nodes:
-            if n.value == node_value:
+            if n.id == node_id:
                 return n
         return NullNode()
 
-    def delete_node(self, node_value):
+    def delete_node(self, node_id):
+        if not node_id in self._nodes_table:
+            GraphException('Node {} not found'.format(node_id))
         for n in self.nodes:
-            if node_value in n:
-                del n.neighbors[node_value]
-        del self.nodes[node_value]
+            if node_id in n:
+                del n.neighbors[node_id]
+        del self.nodes[node_id]
 
     def __contains__(self, node):
         for n in self.nodes:
@@ -75,3 +78,27 @@ class Graph(object):
         return iter(self.nodes.values())
 
 
+class GraphAdjacencyTable(object):
+    def __init__(self):
+        self._number_of_nodes = 0
+        self._nodes_table = {}
+
+    def add_node(self, node_id):
+        if node_id not in self._nodes_table.keys():
+            self._nodes_table[node_id] = {}
+
+    def add_edge_directed(self, source_id, target_id, weight=1):
+        if target_id not in self._nodes_table[source_id].keys():
+            self._nodes_table[source_id] = {target_id: weight}
+
+    def add_edge(self, source_id, target_id, weight=1):
+        self.add_edge_directed(source_id, target_id)
+        self.add_edge_directed(target_id, source_id)
+
+    def delete_node(self, node_id):
+        if not node_id in self._nodes_table:
+            GraphException('Node {} not found'.format(node_id))
+        for n in self._nodes_table:
+            if node_id in n.keys():
+                del n[node_id]
+        del self._nodes_table[node_id]
